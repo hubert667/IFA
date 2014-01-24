af905651ae3eb08cb5d10f105ad40dfd37605553
AlbertoEAF <albertoeaf@gmail.com>
2014-01-24 21:00:15 +0100

    Log-space implementation

--- IFA/hmm_log.py
+++ IFA/hmm_log.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 14 02:25:10 2014
+Created on Fri Jan 24 19:50:09 2014
 
 @author: AlbertoEAF
 """
+
 
 import numpy as np
 #import math as mt
 import scipy.stats
 import sys
+
+lol = 22
 
 def Gsample(mean,stddev):
     """ Returns a sample from a normal with parameters mean and stddev. """
@@ -20,41 +23,20 @@ def gauss_prob(x,mean,var):
     """Returns probability of sampling x from the gaussian"""
     return scipy.stats.norm(mean,var).pdf(x)
 
-
-def unmix(G, Y):
-    """ Unmixes the sound recordings with the unmixing matrix G and returns the estimated X values. """
-    X = np.dot(G,Y)
-    return X
-
-def AbsTotalError(A,B):
-    """ Returns the absolute elementwise error between 2 matrices. """
-    return np.sum(np.abs(A-B))
-
-def Calc_G (G, hmms, X):
-    """Returns a new G matrix after update. Each column of X contain data from different sources for the same timestep """
-    T = X.shape[1]
+def logsumexp(a):
+    B = np.max(a)
     
-    Sum=0    
-    for t in range(T):
-        phi = Calc_phi(hmms,t,X[:,t])
-        result=np.reshape(phi,(phi.shape[0],1))*X[:,t].T
-        Sum += np.dot(result,G)
-    G += Eps*(G-Sum/T)
-    return G
+    return B + np.log(np.sum(np.exp(a-B)))
+
+def e(s=1):
+    sys.exit(s)
+
+def p(a):
+    print a
+    e(0)
     
-def Calc_phi(hmms,t,X):
-    """Calculates phi for X for particular timestep for all HMMs"""
-    phi = np.zeros(X.shape[0])
-
-    for i in range(X.shape[0]):
-        phi[i] = np.sum(hmms[i].gamma[:,t]*(X[i]-hmms[i].mu_states[:])/hmms[i].var_states[:])
-
-        if np.isnan(phi[i]):
-            print "phi[i]=nan", hmms[i].var_states[:], hmms[i].gamma[:,t]*(X[i]-hmms[i].mu_states[:])
-
-    return phi
-
-Eps=0.1 #learning rate for the G matrix
+def W():
+    return raw_input()
 
 class HMM:
     def __init__(self, states, length, mu_init=None, var_init=None):
@@ -73,57 +55,58 @@ class HMM:
         if mu_init!=None and var_init!=None:
             print "Overriding random initialization..."
         
-        self.alpha = np.empty((states, length))        
-        self.beta  = np.ones((states, length))
+        self.log_alpha = np.empty((self.S, self.T))        
+        self.log_beta  = np.zeros((self.S, self.T))
         
-        self.c = np.empty(length)
         
-        self.pi = np.ones(states)  / states # uniform prior   
         
-        self.a = np.ones((states,states)) / states # rows: s', cols: s
+        self.pi = np.ones(self.S)  / self.S # uniform prior   
+        
+        self.T = np.ones((self.S,self.S)) / self.S # rows: s', cols: s
       
-        self.xi_sum_t = np.empty((states,states), dtype=float)      
+        self.xi_sum_t = np.empty((self.S,self.S), dtype=float)      
       
         self.s_range = np.arange(self.S)
       
     def _calc_alphas(self,x):
         # t=1 (t = 0)
-        self.alpha[:,0] = self.pi * gauss_prob(x[0], self.mu_states, self.var_states)
+        self.log_alpha[:,0] = np.log(self.pi) + np.log( gauss_prob(x[0], self.mu_states, self.var_states) )
         
-        self.c[0] = np.sum(self.alpha[:,0])
-        self.alpha[:,0] /= self.c[0]
+            
+        
+        
+        #p(self.log_alpha[:,0])
         
         # t=2,...,T (t = 1,...,T-1)
         for t in range(1, self.T):
             for s in self.s_range:
-                x_prob = gauss_prob(x[t], self.mu_states[s], self.var_states[s])
-                self.alpha[s,t] = x_prob * np.dot(self.alpha[:,t-1], self.a[:,s])
-                
-            self.c[t] = np.sum(self.alpha[:,t])
-            self.alpha[:,t] /= self.c[t]
+                self.log_alpha[s,t] = np.log(gauss_prob(x[t],self.mu_states[s],self.var_states[s])) + logsumexp(self.log_alpha[:,t-1] + np.log(self.T[:,s]))
+        
                 
     def _calc_betas(self,x):
         # t = T (t = T-1)
-        # self.beta[:,self.T-1] = 1 # already done by the initialization
+        # self.log_beta[:,self.T-1] = 0 # already done by the initialization
         
         # t = 1,...,T-1 (t = 0,...,T-2)
         for t in range(self.T-2, -1, -1):
             for s in self.s_range:
-                self.beta[s,t] = np.dot(self.beta[:,t+1], gauss_prob(x[t+1], self.mu_states, self.var_states) * self.a[s,:])
-            self.beta[:,t] /= self.c[t+1]
-                
-    def _calc_gamma(self):
-        self.gamma = self.alpha * self.beta
+                self.log_beta[s,t] = logsumexp(self.log_beta[:,t+1] + np.log(gauss_prob(x[t+1],self.mu_states,self.var_states)) + np.log(self.T[s]))
+            
+            
         
-    def _calc_xi(self, x):
+        
+    def _calc_gamma(self):
+        self.log_gamma = self.log_alpha + self.log_beta
+        
+    def _calc_xi_sum_t(self, x):
         for s in self.s_range:        
             for s_prime in self.s_range:
                 # the sum of xi_s's is the same as the unnormalized a_s's
-                self.xi_sum_t[s_prime,s] = np.sum(self.xi(x, s_prime, s, np.arange(1,self.T))) 
+                self.xi_sum_t[s_prime,s] = np.sum(np.exp(self.log_xi(x, s_prime, s, np.arange(1,self.T))))
     
-    def xi(self, x, s_prime, s, t):
+    def log_xi(self, x, s_prime, s, t):
         """ IMPORTANT!: t>0 """           
-        return self.alpha[s_prime,t-1]*self.beta[s,t]*gauss_prob(x[t],self.mu_states[s],self.var_states[s])*self.a[s_prime,s] / self.c[t]
+        return self.log_alpha[s_prime,t-1] + self.log_beta[s,t] + np.log(gauss_prob(x[t],self.mu_states[s],self.var_states[s])) + np.log(self.T[s_prime,s])
         
 
     def update(self,x):
@@ -135,32 +118,29 @@ class HMM:
         self._calc_betas(x)
         
         self._calc_gamma()
-        self._calc_xi(x)
+        self._calc_xi_sum_t(x) # only the sum of xi for all time is relevant
         
         # M-step        
         
         for s in self.s_range:
-            sum_gamma_s = np.sum(self.gamma[s])
+            sum_gamma_s = np.sum(np.exp(self.log_gamma[s])) # with np.exp(logsumexp(self.log_gamma[s])) doesn't work either
             
-            self.mu_states[s] = np.dot(self.gamma[s], x) / sum_gamma_s
+            self.mu_states[s] = np.dot(np.exp(self.log_gamma[s]), x) / sum_gamma_s
             
-            self.var_states[s]= np.dot(self.gamma[s], np.power(x-self.mu_states[s],2)) / sum_gamma_s
+            self.var_states[s]= np.dot(np.exp(self.log_gamma[s]), np.power(x-self.mu_states[s],2)) / sum_gamma_s
                
         # the sum of xi_s's is the same as the unnormalized a_s's
-        self.a = np.copy(self.xi_sum_t) # depending on the situation / upgrade to the final algorithm, copy may be avoided
-        for s_prime     in self.s_range:    
-            self.a[s_prime] /= np.sum(self.a[s_prime])
+        self.T = np.copy(self.xi_sum_t) # depending on the situation / upgrade to the final algorithm, copy may be avoided
+        for s_prime in self.s_range:    
+            self.T[s_prime] /= np.sum(self.T[s_prime])
             
         
-        self.pi = self.gamma[:,0] / np.sum(self.gamma[:,0])
+        self.pi = np.exp(self.log_gamma[:,0]) / np.sum(np.exp(self.log_gamma[:,0]))
         
         
-
-    def likelihood(self):
-        return np.prod(self.c)
         
     def log_likelihood(self):
-        return np.sum(np.log(self.c))
+        return logsumexp(self.log_alpha[:,-1])
 
 
 def discretePDF2CDF(w):
@@ -168,8 +148,7 @@ def discretePDF2CDF(w):
         w[i] += w[i-1]
     return w/w[-1] # normalizes in case the input wasn't
 
-def e(s=1):
-    sys.exit(s)
+
 
 def discreteInvCDF(CDF,x):
     " Returns the class of the CDF to which x belongs. "
@@ -236,11 +215,13 @@ w = [.4, 0.4]
 mu_w = [0., 50.]
 var_w = [3., 4.]
 
-#x = np.array([ Gsample(0,5) for i in range(T) ])
-x = np.asarray([ Gsample(20.,3) for i in range(T/2) ] + [ Gsample(50,4) for i in range(T/2) ]) # requires even T
+x = np.array([ Gsample(0,5) for i in range(T) ])
+#x = np.asarray([ Gsample(20.,3) for i in range(T/2) ] + [ Gsample(50,4) for i in range(T/2) ]) # requires even T
 #x = GSeqSample(T, .999, w, mu_w, var_w) # persistence of 0.8 creates reasonably noticeable chains when w is close to uniform
 
 iterations = 20
+
+print "lol", lol
 
 log_likelihoods = []
 for i in range(iterations):
