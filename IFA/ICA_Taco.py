e91fe97e3c61fd9a2f0a4c52dc653ab29a5fef06
AlbertoEAF <albertoeaf@gmail.com>
2014-01-30 16:53:13 +0100

    Testing Our and Taco's in parallel

--- IFA/ICA_Taco.py
+++ IFA/ICA_Taco.py
@@ -19,13 +19,31 @@ Created on Wed Jan 29 15:12:33 2014
 @author: AlbertoEAF
 """
 
-from hmm import *
+#from hmm import *
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
+import sys
 
-activation_functions = [lambda a: -tanh(a),    lambda a: -a + tanh(a),                 lambda a: -a ** 3,             lambda a: - (6 * a) / ( a ** 2 + 5.0)]
-p_from_act_functions = [lambda a: 1 / cosh(a), lambda a: cosh(a) * exp(-0.5 * a ** 2), lambda a: exp(-0.25 * a ** 4), lambda a: (a ** 2 + 5.0) ** (-3)     ]
+def e(i):
+    sys.exit(i)
+
+activation_functions = [lambda a: -np.tanh(a),    lambda a: -a + np.tanh(a),                 lambda a: -a ** 3.0,             lambda a: - (6 * a) / ( a ** 2.0 + 5.0)]
+p_from_act_functions = [lambda a: 1 / np.cosh(a), lambda a: np.cosh(a) * np.exp(-0.5 * a ** 2.), lambda a: np.exp(-0.25 * a ** 4.), lambda a: (a ** 2. + 5.0) ** (-3.)     ]
+
+
+def dAmari(W,W0):
+    """ 
+    Returns the Amari distance between two permutation-invariant matrices and row-scale invariant. 
+    W0 is the perfect unmixing matrix and W the estimated one. 
+    """
+    r = np.abs(np.dot(W0,np.linalg.inv(W))) # L1 element-wise norm?
+        
+    d1 = np.sum(np.sum(r,axis=1)/np.max(r,axis=1) - 1)
+    d2 = np.sum(np.sum(r,axis=0)/np.max(r,axis=0) - 1)
+    
+    return 1/(2*float(r.shape[0])) * (d1 + d2)
+
 
 def GetData(filepath, start=0, size=100, plot_hist=1, hist_bins=50):
     """ Gets normalized amplitude data from a .wav file. """
@@ -55,9 +73,10 @@ def GetData(filepath, start=0, size=100, plot_hist=1, hist_bins=50):
     return wave_data
 
 
-def ICA(X, perfectW, activation_function, learning_rate=0.01, max_iterations = 1000000):
+def ICA(X, W0, activation_function=lambda a: -np.tanh(a), learning_rate=0.01, max_iterations = 1000000):
    
-    W = 0.01 * numpy.random.rand(X.shape[0],X.shape[0])
+    W = 1e-2 * np.random.rand(X.shape[0],X.shape[0])
+    
   
     error = [np.inf]  
   
@@ -70,20 +89,54 @@ def ICA(X, perfectW, activation_function, learning_rate=0.01, max_iterations = 1
         W += learning_rate * dW
        
         Wsum = np.absolute(dW).sum()
+              
        
-        print W, perfectW
+        print W/np.sum(W,axis=0)
         
-        error.append(np.linalg.norm(W-perfectW))     
+        error.append(dAmari(W,W0))
+
         if error[-1] - error[-2] > 0.00001:
             print "Error increasing!"
             break       
-        if numpy.isnan(Wsum) or numpy.isinf(Wsum):
+        if np.isnan(Wsum) or np.isinf(Wsum):
             print "Failed convergence!"
             break
         if np.linalg.norm(dW) < 1e-5:
             print "Early solution! :)"
             break
  
+    return W, error
+
+def ICATaco(X, W0, activation_function=lambda a: -np.tanh(a), learning_rate=0.01, stop = 1e-5):
+    """ W0 is the perfect unmixing matrix. """    
+    
+    W= 1e-2 * np.random.rand(X.shape[0], X.shape[0]);
+    
+    print "Running Taco's ICA"
+    
+    error = [np.inf]    
+    
+    changeW = 9999
+    i=0
+    while changeW > stop:
+        i+=1
+        if i%1000==0:
+            print i, changeW
+
+        a = np.dot(W, X)
+        z = activation_function(a)
+        x_prime = dot(W.T, a)
+        Wgrad = W + np.dot(z, x_prime.T) / X.shape[1]
+        
+        W += learning_rate * Wgrad
+        
+        error.append(dAmari(W,W0))
+        if error[-1]-error[-2] > 0.1:
+            print "Error increased"
+            break
+        
+        changeW = np.linalg.norm(Wgrad)
+
     return W, error
 
 S = 2 # states
@@ -101,14 +154,20 @@ X[0] = GetData("mike.wav", 6000, T)
 X[1] = GetData("beet.wav", 6000, T)
 y = np.dot(H,X)
 
-G,eG = ICA(y, np.linalg.inv(H), activation_functions[1])
+G,eG = ICA(y, np.linalg.inv(H))#, activation_functions[3])
+G2,eG2 = ICATaco(y,np.linalg.inv(H))
 
 plt.figure()
 plt.plot(eG)
-
 plt.xlabel("Iterations")
 plt.ylabel("G error $| G - H^{-1}|$")
 
-print G/G[0,0]
+plt.figure()
+plt.plot(eG2)
+plt.xlabel("Iterations")
+plt.ylabel("G error $| G - H^{-1}|$")
 
+
+print G/np.sum(G,axis=0)
+print G2/np.sum(G2,axis=0)
 e(0)
